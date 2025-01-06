fd336d1b27671d2e558dc945435da10c3140885e
Jlwh747 <juanital.white@outlook.com>
2025-01-06 14:41:14 +0300

    Commit: Add Streamlit app

--- app.py
+++ app.py
@@ -1,4 +1,17 @@
 import pandas as pd
 import streamlit as st
 import plotly.express as px
-print ('hello world')
+
+df = pd.read_csv('vehicles_us.csv')
+st.header('Vehicle Data Dashboard')
+
+histogram = px.histogram(df,x='model_year',title ='model year of car')
+st.plotly_chart(histogram)
+
+
+scatter = px.scatter(df,x='model_year',y='condition', title ='model_year vs. condition')
+st.plotly_chart(scatter)
+
+if st.checkbox("Show Dataset"):
+    st.write(df.head())
+
